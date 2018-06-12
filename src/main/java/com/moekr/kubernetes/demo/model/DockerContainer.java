312a7693d60542a2df456f6b6157e35da4ede376
Moekr <highter@outlook.com>
2018-06-12 12:19:03 +0800

    加上环境变量

--- src/main/java/com/moekr/kubernetes/demo/model/DockerContainer.java
+++ src/main/java/com/moekr/kubernetes/demo/model/DockerContainer.java
@@ -1,17 +1,24 @@
 package com.moekr.kubernetes.demo.model;
 
 import io.fabric8.kubernetes.api.model.Container;
+import io.fabric8.kubernetes.api.model.EnvVar;
 import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Data
 public class DockerContainer {
 	private String name;
 	private String image;
+	private Map<String, String> env = new HashMap<>();
 
 	public Container toContainer() {
 		Container container = new Container();
 		container.setName(name);
 		container.setImage(image);
+		container.setEnv(env.entrySet().stream().map(e -> new EnvVar(e.getKey(), e.getValue(), null)).collect(Collectors.toList()));
 		return container;
 	}
 }
